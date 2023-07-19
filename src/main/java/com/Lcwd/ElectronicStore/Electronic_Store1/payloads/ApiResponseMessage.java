e49aeddf88b853f906b29761808854d44eaab8bd
Pooja Kathar <poojakathar11@gmail.com>
2023-07-19 05:01:43 -0700

    User Model ApiResponses done

---
+++ src/main/java/com/Lcwd/ElectronicStore/Electronic_Store1/payloads/ApiResponseMessage.java
@@ -0,0 +1,18 @@
+package com.Lcwd.ElectronicStore.Electronic_Store1.payloads;
+
+import lombok.*;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ApiResponseMessage {
+
+    private String message;
+    private boolean success;
+    private HttpStatus status;
+    }
+
+
