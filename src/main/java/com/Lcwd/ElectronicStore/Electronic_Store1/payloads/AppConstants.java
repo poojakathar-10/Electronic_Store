1ca524e89e1a14b9f5f112e18cfbc60dcaa74752
Pooja Kathar <poojakathar11@gmail.com>
2023-07-19 05:00:25 -0700

    User Model constants done

---
+++ src/main/java/com/Lcwd/ElectronicStore/Electronic_Store1/payloads/AppConstants.java
@@ -0,0 +1,7 @@
+package com.Lcwd.ElectronicStore.Electronic_Store1.payloads;
+
+public class AppConstants {
+
+    public static final String NOT_FOUND="User not found with given id";
+    public static final String DELETE_USER="User is deleted sucessfully";
+}
